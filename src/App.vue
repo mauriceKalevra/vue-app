db4b60151b9b60cdd19d4ec0b3c161c53c2b8d03
Maurice <Mori.r@web.de>
2022-10-10 11:05:53 +0200

    vue init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+{{name}},
+{{age}},
+{{ `${height} ${IQ}` }} <hr>
+{{fullid}}
+<hr>
+{{age >18 ? 'adult' : 'kid' }},
+<div class="agee">
+{{age}}
+<span v-if="age < 18">
+You're under 18.
+</span>
+<span v-else-if="age ==18">
+You're 18 years old
+</span>
+<span v-else>
+Passed, You're above 18 years old!
+</span>
+</div>
+<div v-show="name == 'Maurice'">
+Hi, Maurice!
+</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () =>({
+    name: 'Mauricee',
+    age: 185,
+    height: 183,
+    IQ: 138,
+    Cock: 16
+  }),
+  computed: {
+    fullid() {
+      return this.name + " " + this.age;
+    }
   }
 }
 </script>
 
 <style>
+.agee {
+  border: 2px solid #f00;
+  margin: 5px;
+  padding: 10px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
